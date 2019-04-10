ac16d52286b976b7e09501b12c13c702f7658f28
Jay J <mis@thewoodagency.com>
2019-04-10 07:13:19 -0500

    add blog route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/blog', function () {
+    return view('blog');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');

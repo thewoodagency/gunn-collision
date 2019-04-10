8060a4c72e114218b33ff1e381283a91bfad0970
Jay J <mis@thewoodagency.com>
2019-04-10 13:58:33 -0500

    added layout blade

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,48 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>MyBlog | My Awesome Blog</title>
-
-    <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/custom.css">
-</head>
-<body>
-    <header>
-        <nav class="navbar navbar-default navbar-fixed-top">
-          <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#the-navbar-collapse" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">MyBlog</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="the-navbar-collapse">
-              <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="index.html">Blog</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
-            </div><!-- /.navbar-collapse -->
-          </div><!-- /.container -->
-        </nav>
-    </header>
-
+@extends ('layouts.blog')
+@section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8">
                 <article class="post-item post-detail">
                     <div class="post-item-image">
                         <a href="#">
-                            <img src="img/Post_Image_1.jpg" alt="">
+                            <img src="/img/Post_Image_1.jpg" alt="">
                         </a>
                     </div>
 
@@ -72,21 +36,21 @@
 
                 <article class="post-author padding-10">
                     <div class="media">
-                      <div class="media-left">
-                        <a href="#">
-                          <img alt="Author 1" src="img/author.jpg" class="media-object">
-                        </a>
-                      </div>
-                      <div class="media-body">
-                        <h4 class="media-heading"><a href="#">Masaru Edo</a></h4>
-                        <div class="post-author-count">
-                          <a href="#">
-                              <i class="fa fa-clone"></i>
-                              90 posts
-                          </a>
+                        <div class="media-left">
+                            <a href="#">
+                                <img alt="Author 1" src="/img/author.jpg" class="media-object">
+                            </a>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad aut sunt cum, mollitia excepturi neque sint magnam minus aliquam, voluptatem, labore quis praesentium eum quae dolorum temporibus consequuntur! Non.</p>
-                      </div>
+                        <div class="media-body">
+                            <h4 class="media-heading"><a href="#">Masaru Edo</a></h4>
+                            <div class="post-author-count">
+                                <a href="#">
+                                    <i class="fa fa-clone"></i>
+                                    90 posts
+                                </a>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad aut sunt cum, mollitia excepturi neque sint magnam minus aliquam, voluptatem, labore quis praesentium eum quae dolorum temporibus consequuntur! Non.</p>
+                        </div>
                     </div>
                 </article>
 
@@ -197,140 +161,10 @@
 
                 </article>
             </div>
-            <div class="col-md-4">
-                <aside class="right-sidebar">
-                    <div class="search-widget">
-                        <div class="input-group">
-                          <input type="text" class="form-control input-lg" placeholder="Search for...">
-                          <span class="input-group-btn">
-                            <button class="btn btn-lg btn-default" type="button">
-                                <i class="fa fa-search"></i>
-                            </button>
-                          </span>
-                        </div><!-- /input-group -->
-                    </div>
-
-                    <div class="widget">
-                        <div class="widget-heading">
-                            <h4>Categories</h4>
-                        </div>
-                        <div class="widget-body">
-                            <ul class="categories">
-                                <li>
-                                    <a href="#"><i class="fa fa-angle-right"></i> Web Development</a>
-                                    <span class="badge pull-right">10</span>
-                                </li>
-                                <li>
-                                    <a href="#"><i class="fa fa-angle-right"></i> Web Design</a>
-                                    <span class="badge pull-right">10</span>
-                                </li>
-                                <li>
-                                    <a href="#"><i class="fa fa-angle-right"></i> General</a>
-                                    <span class="badge pull-right">10</span>
-                                </li>
-                                <li>
-                                    <a href="#"><i class="fa fa-angle-right"></i> DIY</a>
-                                    <span class="badge pull-right">10</span>
-                                </li>
-                                <li>
-                                    <a href="#"><i class="fa fa-angle-right"></i> Facebook Development</a>
-                                    <span class="badge pull-right">10</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="widget">
-                        <div class="widget-heading">
-                            <h4>Popular Posts</h4>
-                        </div>
-                        <div class="widget-body">
-                            <ul class="popular-posts">
-                                <li>
-                                    <div class="post-image">
-                                        <a href="#">
-                                            <img src="img/Post_Image_5_thumb.jpg" />
-                                        </a>
-                                    </div>
-                                    <div class="post-body">
-                                        <h6><a href="#">Blog Post #5</a></h6>
-                                        <div class="post-meta">
-                                            <span>36 minutes ago</span>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="post-image">
-                                        <a href="#">
-                                            <img src="img/Post_Image_4_thumb.jpg" />
-                                        </a>
-                                    </div>
-                                    <div class="post-body">
-                                        <h6><a href="#">Blog Post #4</a></h6>
-                                        <div class="post-meta">
-                                            <span>36 minutes ago</span>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="post-image">
-                                        <a href="#">
-                                            <img src="img/Post_Image_3_thumb.jpg" />
-                                        </a>
-                                    </div>
-                                    <div class="post-body">
-                                        <h6><a href="#">Blog Post #3</a></h6>
-                                        <div class="post-meta">
-                                            <span>36 minutes ago</span>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="widget">
-                        <div class="widget-heading">
-                            <h4>Tags</h4>
-                        </div>
-                        <div class="widget-body">
-                            <ul class="tags">
-                                <li><a href="#">PHP</a></li>
-                                <li><a href="#">Codeigniter</a></li>
-                                <li><a href="#">Yii</a></li>
-                                <li><a href="#">Laravel</a></li>
-                                <li><a href="#">Ruby on Rails</a></li>
-                                <li><a href="#">jQuery</a></li>
-                                <li><a href="#">Vue Js</a></li>
-                                <li><a href="#">React Js</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </aside>
-            </div>
+            @include('layouts.sidebar')
         </div>
     </div>
+@endsection
 
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <p class="copyright">&copy; 2016 Edo Masaru</p>
-                </div>
-                <div class="col-md-4">
-                    <nav>
-                        <ul class="social-icons">
-                            <li><a href="#" class="i fa fa-facebook"></a></li>
-                            <li><a href="#" class="i fa fa-twitter"></a></li>
-                            <li><a href="#" class="i fa fa-google-plus"></a></li>
-                            <li><a href="#" class="i fa fa-github"></a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </footer>
 
-    <script src="js/bootstrap.min.js"></script>
-</body>
-</html>
+
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('/blog', function () {
     return view('blog.index');
 });
 
+Route::get('/blog/show', function () {
+    return view('blog.show');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
